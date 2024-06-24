7f47eec8ef74b1c8d806fea1a45f18728f58cc57
Imeanbusiness <dominic.adam.koh@gmail.com>
2024-06-24 16:38:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const shanplay = new Audio("indian.mp3")
 
 function start() {
-    alert("Shanvanth is eh")
+    alert("Hi! I'm Shanvanth. I wrote this part, and here is a fun fact: My avergae KDA is 3 / 132 / 4\nAnd that's when I play with bots!")
+
 }
 
